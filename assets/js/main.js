eeb6704e5daa2c625d376c61f4cf5719c91e6a83
Окулов Семён <67464545+scffs@users.noreply.github.com>
2022-12-16 10:11:07 +0700

    renamed classes etc

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,11 @@ const advantagesList = [
         titleClass: '',
         text: 'Тебе больше не нужно сидеть в ожидании пока появится номер, нажал на кнопку <strong>«Автодозвон»</strong>и занимаешься своими делами. (Работает только на ноутбуке или компьютере)',
         imgUrl: 'assets/images/advantages/hands.svg"',
-        class:'ItemAdvantages_DecorRed'
+        class:'item-advantages_decor_red'
     },
     {
         title: 'Привяжем номер',
-        titleClass: 'ItemAdvantages-Title_Free',
+        titleClass: 'item-advantages__title_free',
         text: 'телефона с которого будешь звонить! <strong>Бесплатные звонки</strong> на номера авито<strong>без блокировки!</strong>Больше не нужно менять симку!',
         imgUrl: 'assets/images/advantages/mail.svg"',
         class:''
@@ -18,34 +18,34 @@ const advantagesList = [
         titleClass: '',
         text: 'Если ты хочешь звонить с компьютера через интернет, то мы поможем это сделать! Звонки через интернет проходят <strong>быстрее чем обычные</strong>, к тому же, <strong>они в разы дешевле!</strong>Будь на шаг впереди!',
         imgUrl: 'assets/images/advantages/support.svg"',
-        class:'ItemAdvantages_DecorBlue'
+        class:'item-advantages_decor_blue'
     },
     {
         title: 'Выделение цветом',
         titleClass: '',
         text: 'Можешь <strong>выделять цветом</strong> объявления, одно нажатие - синий, два нажатия - красный. Помогает не путаться в информации и не терять концентрацию!',
         imgUrl: 'assets/images/advantages/color.svg"',
-        class:'ItemAdvantages_DecorLine'
+        class:'item-advantages_decor_line'
     },
     {
         title: 'Фильтры',
         titleClass: '',
         text: 'Можно выбрать параметры, цену или просмотры, и отобразить в программе только нужные тебе объявления! А так же много других <strong>функций</strong>, которые помогут тебе в работе!',
         imgUrl: 'assets/images/advantages/filters.svg"',
-        class:'ItemAdvantages_DecorPurpleGreen'
+        class:'item-advantages_decor_purple-green'
     }
 ]
-const advantagesItem = document.querySelector('.Advantages-List')
+const advantagesItem = document.querySelector('.advantages__list')
 advantagesItem.innerHTML = advantagesList.map(
     item=>(
-        `<li class="Advantages-Item ItemAdvantages ${item.class}">
-                            <div class="ItemAdvantages-Info">
-                                <h3 class="ItemAdvantages-Title ${item.titleClass}">${item.title}</h3>
-                                ${item.class === 'ItemAdvantages_DecorRed' ? '<h4 class="ItemAdvantages-SubTitle">входит в стоимость подписки!</h4>' : ''}
-                                <p class="ItemAdvantages-Text">${item.text}</p>
+        `<li class="advantages__item item-advantages ${item.class}">
+                            <div class="item-advantages__Info">
+                                <h3 class="item-advantages__title ${item.titleClass}">${item.title}</h3>
+                                ${item.class === 'item-advantages_decor_red' ? '<h4 class="item-advantages__sub-title">входит в стоимость подписки!</h4>' : ''}
+                                <p class="item-advantages__text">${item.text}</p>
                             </div>
-                            <div class="ItemAdvantages-ImgWrapper">
-                                <img class="ItemAdvantages-Img" src="${item.imgUrl}" alt="Автодозвон">
+                            <div class="item-advantages__img-wrapper">
+                                <img class="item-advantages__img" src="${item.imgUrl}" alt="Автодозвон">
                             </div>
                         </li>`
     )
